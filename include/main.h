9b4abfd9052a3c94a311b769e741992e083be1d5
Jonathan Chiang <jonathan.z.chiang@gmail.com>
2023-07-09 15:01:56 -0400

    Set up directory structure

---
+++ include/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <iostream>
+
+namespace lidarslam {
+	void print_test() {
+		std::cout << "Print Test" << std::endl;
+	}
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "main.h"
 
 using namespace std;
 int main(int argc, char* argv[]){
 	cout << "Hello World!" << endl;
+	lidarslam::print_test();
 	return 0;
 }
