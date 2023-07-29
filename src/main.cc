ef39488f992f24c7f95dbe23efd4bdeb426e002c
Jonathan Chiang <jonathan.z.chiang@gmail.com>
2023-07-29 02:11:33 -0400

    Include Eigen

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,7 @@
 #include <iostream>
+
+// #include <Eigen/Dense>
+
 #include "io.h"
 #include "real_map.h"
 #include "main.h"
@@ -6,7 +9,7 @@
 using namespace std;
 int main(int argc, char* argv[]) {
     lidarslam::ElevationMap elevation_map = lidarslam::read_map("dat/map.dat");
-    // elevation_map.print();
     std::cout << elevation_map(-0.5, 0.5) << std::endl;
+		// Eigen::Vector3d vec;
     return 0;
 }
